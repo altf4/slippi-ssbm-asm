267b9bcfcb253b9114e9b6f87ae3665236108134
AltF4 <altf4petro@gmail.com>
2022-07-24 19:05:34 -0700

    Changed r3 to r31, should be safer that way

--- Recording/SendMenuFrame.asm
+++ Recording/SendMenuFrame.asm
@@ -22,7 +22,7 @@
 backup STACK_FREE_SPACE
 
 # Allocate ourselves a buffer
-li  r3, EXI_BUF_LEN+64
+li  r31, EXI_BUF_LEN+64
 branchl r12,HSD_MemAlloc
 
 # check if NOT VS Mode
@@ -32,14 +32,14 @@ beq Injection_Exit
 cmpwi r8, 0x0208
 beq Injection_Exit
 
-# addi r3, sp, STACK_OFST_EXI_BUF # This is the start address for the free space
-# byteAlign32 r3, r3
+# addi r31, sp, STACK_OFST_EXI_BUF # This is the start address for the free space
+# byteAlign32 r31, r31
 
 li r4, CMD_MENU_FRAME # Command byte
-stb r4, 0x0(r3)
+stb r4, 0x0(r31)
 
 # Two bytes for major / minor scene
-sth r8, 0x1(r3)
+sth r8, 0x1(r31)
 
 # send player 1 cursor x position
 # Each player has a heap-allocated struct, make sure the ptr is not NULL before reading
@@ -51,17 +51,17 @@ bne SendP1Cursor
 
 # set cursor values to 0
 load r5, 0x00000000
-stw r5, 0x3(r3)
-stw r5, 0x7(r3)
+stw r5, 0x3(r31)
+stw r5, 0x7(r31)
 b P2_Cursor
 
 SendP1Cursor:
 # Load cursor x position
 lwz r5, 0x0c(r4)
-stw r5, 0x3(r3)
+stw r5, 0x3(r31)
 # Load cursor y position
 lwz r5, 0x10(r4)
-stw r5, 0x7(r3)
+stw r5, 0x7(r31)
 
 P2_Cursor:
 load r4, CSS_CURSOR_STRUCT_PTR_P2
@@ -71,17 +71,17 @@ bne SendP2Cursor
 
 # set cursor values to 0
 load r5, 0x00000000
-stw r5, 0xB(r3)
-stw r5, 0xF(r3)
+stw r5, 0xB(r31)
+stw r5, 0xF(r31)
 b P3_Cursor
 
 SendP2Cursor:
 # Load cursor x position
 lwz r5, 0x0c(r4)
-stw r5, 0xB(r3)
+stw r5, 0xB(r31)
 # Load cursor y position
 lwz r5, 0x10(r4)
-stw r5, 0xF(r3)
+stw r5, 0xF(r31)
 
 P3_Cursor:
 load r4, CSS_CURSOR_STRUCT_PTR_P3
@@ -91,17 +91,17 @@ bne SendP3Cursor
 
 # set p1 cursor values to 0
 load r5, 0x00000000
-stw r5, 0x13(r3)
-stw r5, 0x17(r3)
+stw r5, 0x13(r31)
+stw r5, 0x17(r31)
 b P4_Cursor
 
 SendP3Cursor:
 # Load cursor x position
 lwz r5, 0x0c(r4)
-stw r5, 0x13(r3)
+stw r5, 0x13(r31)
 # Load cursor y position
 lwz r5, 0x10(r4)
-stw r5, 0x17(r3)
+stw r5, 0x17(r31)
 
 P4_Cursor:
 load r4, CSS_CURSOR_STRUCT_PTR_P4
@@ -111,17 +111,17 @@ bne SendP4Cursor
 
 # set p1 cursor values to 0
 load r5, 0x00000000
-stw r5, 0x1B(r3)
-stw r5, 0x1F(r3)
+stw r5, 0x1B(r31)
+stw r5, 0x1F(r31)
 b CURSORS_DONE
 
 SendP4Cursor:
 # Load cursor x position
 lwz r5, 0x0c(r4)
-stw r5, 0x1B(r3)
+stw r5, 0x1B(r31)
 # Load cursor y position
 lwz r5, 0x10(r4)
-stw r5, 0x1F(r3)
+stw r5, 0x1F(r31)
 
 CURSORS_DONE:
 
@@ -131,12 +131,12 @@ CURSORS_DONE:
 #   value of 0 is fully visible (ready to play)
 load r4 0x804d6cf2
 lbz r4, 0(r4)
-stb r4, 0x23(r3)
+stb r4, 0x23(r31)
 
 # Stage selected (one byte)
 load r4 0x804D6CAD
 lbz r4, 0(r4)
-stb r4, 0x24(r3)
+stb r4, 0x24(r31)
 
 # controller port statuses at CSS (each one byte)
 # 0 == Human
@@ -145,37 +145,37 @@ stb r4, 0x24(r3)
 # Player 1
 load r4 0x803F0E08
 lbz r4, 0(r4)
-stb r4, 0x25(r3)
+stb r4, 0x25(r31)
 # Player 2
 load r4 0x803F0E2C
 lbz r4, 0(r4)
-stb r4, 0x26(r3)
+stb r4, 0x26(r31)
 # Player 3
 load r4 0x803F0E50
 lbz r4, 0(r4)
-stb r4, 0x27(r3)
+stb r4, 0x27(r31)
 # Player 4
 load r4 0x803F0E74
 lbz r4, 0(r4)
-stb r4, 0x28(r3)
+stb r4, 0x28(r31)
 
 # Character selected (each one byte)
 # Player 1
 load r4 0x803F0E0A
 lbz r4, 0(r4)
-stb r4, 0x29(r3)
+stb r4, 0x29(r31)
 # Player 2
 load r4 0x803F0E2E
 lbz r4, 0(r4)
-stb r4, 0x2A(r3)
+stb r4, 0x2A(r31)
 # Player 3
 load r4 0x803F0E52
 lbz r4, 0(r4)
-stb r4, 0x2B(r3)
+stb r4, 0x2B(r31)
 # Player 4
 load r4 0x803F0E76
 lbz r4, 0(r4)
-stb r4, 0x2C(r3)
+stb r4, 0x2C(r31)
 
 # Coin down
 # 0 == No coin
@@ -189,7 +189,7 @@ stb r4, 0x2C(r3)
 
 # Load 0's into player coins
 load r4 0x00000000
-stw r4, 0x2D(r3)
+stw r4, 0x2D(r31)
 
 cmpwi r8, 0x0002
 bne Not_CSS
@@ -199,25 +199,25 @@ load r4 0x804a0bc0
 lwz r4, 0(r4)
 addi r4, r4, 5
 lbz r4, 0(r4)
-stb r4, 0x2D(r3)
+stb r4, 0x2D(r31)
 # Player 2
 load r4 0x804a0bc4
 lwz r4, 0(r4)
 addi r4, r4, 5
 lbz r4, 0(r4)
-stb r4, 0x2E(r3)
+stb r4, 0x2E(r31)
 # Player 3
 load r4 0x804a0bc8
 lwz r4, 0(r4)
 addi r4, r4, 5
 lbz r4, 0(r4)
-stb r4, 0x2F(r3)
+stb r4, 0x2F(r31)
 # Player 4
 load r4 0x804a0bcc
 lwz r4, 0(r4)
 addi r4, r4, 5
 lbz r4, 0(r4)
-stb r4, 0x30(r3)
+stb r4, 0x30(r31)
 
 Not_CSS:
 
@@ -227,9 +227,9 @@ Not_CSS:
 
 # Load 0's into cursors
 load r4 0x00000000
-stw r4, 0x31(r3)
+stw r4, 0x31(r31)
 load r4 0x00000000
-stw r4, 0x35(r3)
+stw r4, 0x35(r31)
 
 # 0x0102 is offline SSS
 # 0x0108 is online SSS
@@ -251,7 +251,7 @@ addi r4, r4, 0x28
 lwz r4, 0(r4)
 addi r4, r4, 0x38
 lwz r4, 0(r4)
-stw r4, 0x31(r3)
+stw r4, 0x31(r31)
 
 # Stage Select Cursor y
 # 4-byte float
@@ -263,70 +263,68 @@ addi r4, r4, 0x28
 lwz r4, 0(r4)
 addi r4, r4, 0x3C
 lwz r4, 0(r4)
-stw r4, 0x35(r3)
+stw r4, 0x35(r31)
 
 Not_SSS:
 
 # Frame count
 load r4 0x80479D60
 lwz r4, 0(r4)
-stw r4, 0x39(r3)
+stw r4, 0x39(r31)
 
 # Sub-menu
 load r4 0x804A04F0
 lbz r4, 0(r4)
-stb r4, 0x3D(r3)
+stb r4, 0x3D(r31)
 
 # Menu selection index
 load r4 0x804A04F3
 lbz r4, 0(r4)
-stb r4, 0x3E(r3)
+stb r4, 0x3E(r31)
 
 # Online character costume
 load r4 0x803F0E09
 lbz r4, 0(r4)
-stb r4, 0x3F(r3)
+stb r4, 0x3F(r31)
 
 # Is in nametag entry? 0x5 for true
 load r4 0x804d6cf6
 lbz r4, 0(r4)
-stb r4, 0x40(r3)
+stb r4, 0x40(r31)
 
 # CPU Level for all 4 players
 load r4 0x8048082F
 lbz r4, 0(r4)
-stb r4, 0x41(r3)
+stb r4, 0x41(r31)
 load r4 0x80480853
 lbz r4, 0(r4)
-stb r4, 0x42(r3)
+stb r4, 0x42(r31)
 load r4 0x80480877
 lbz r4, 0(r4)
-stb r4, 0x43(r3)
+stb r4, 0x43(r31)
 load r4 0x8048089B
 lbz r4, 0(r4)
-stb r4, 0x44(r3)
+stb r4, 0x44(r31)
 
 # Is Holding CPU Slider for all 4 players
 load r4 0x803F0E0E
 lbz r4, 0(r4)
-stb r4, 0x45(r3)
+stb r4, 0x45(r31)
 load r4 0x803F0E32
 lbz r4, 0(r4)
-stb r4, 0x46(r3)
+stb r4, 0x46(r31)
 load r4 0x803F0E56
 lbz r4, 0(r4)
-stb r4, 0x47(r3)
+stb r4, 0x47(r31)
 load r4 0x803F0E7A
 lbz r4, 0(r4)
-stb r4, 0x48(r3)
+stb r4, 0x48(r31)
 
 #------------- Transfer Buffer ------------
-# r3 is the buffer arg, but it's already set
-mr r31, r3 # backup r3
+mr r3, r31
 li r4, EXI_BUF_LEN
 li r5, CONST_ExiWrite
 branchl r12, FN_EXITransferBuffer
-mr r3, r31 # restore r3
 
 Injection_Exit:
 
