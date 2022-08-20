9f94e3fb82fd305086f8b9655b12dd0cf4a286ce
AltF4 <altf4petro@gmail.com>
2022-08-20 08:41:46 -0700

    SQUASHME: Saving progress

--- Recording/SendGamePostFrame.asm
+++ Recording/SendGamePostFrame.asm
@@ -191,18 +191,18 @@ backup
 
   # FoD Platforms
   # Load the current stage id from the static match block
-  load r12, 0x8046db77
-  lbz r12, 0 (r12)
+  load r4, 0x8046db77
+  lbz r4, 0 (r4)
 
   # Compare with the id for the Fountain of Dreams
-  cmpwi r12, 0x02
+  cmpwi r4, 0x02
   beq Read_Platforms
 
   # We're not on FoD, so zero out the platform locations
   #  then branch away
   load r4 0x00000000
-  stw r4, 0x71(r31)
-  stw r4, 0x75(r31)
+  stw r4, 0x71(REG_Buffer)
+  stw r4, 0x75(REG_Buffer)
   b Read_Platforms_Done
 
 Read_Platforms:
@@ -210,13 +210,13 @@ Read_Platforms:
   load r4 0x804d64b8
   lwz r4, 0(r4)
   addi r4, r4, 0x0c
-  stw r4, 0x71(r31)
+  stw r4, 0x71(REG_Buffer)
 
   # FoD right platform
   load r4 0x804d64b8
   lwz r4, 0(r4)
   addi r4, r4, 0x3c
-  stw r4, 0x75(r31)
+  stw r4, 0x75(REG_Buffer)
 Read_Platforms_Done:
 
 #------------- Increment Buffer Offset ------------
