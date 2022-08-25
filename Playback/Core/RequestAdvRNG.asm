d93a8d35275a5e3ed12cf4f0ffe531e6204aed8a
UnclePunch <vinbertinelli@gmail.com>
2022-08-25 15:28:06 -0400

    add adventure mode RNG restoration

--- Playback/Core/RequestAdvRNG.asm
+++ Playback/Core/RequestAdvRNG.asm
@@ -1,49 +1,51 @@
 ################################################################################
-# Address: 801b5990
+# Address: 801b4398
 ################################################################################
 
 .include "Common/Common.s"
 .include "Playback/Playback.s"
 .include "TASBot.s"
 
-.set REG_MajorData, 26
-.set REG_Buffer, 30
+.set REG_MajorData, 30
+.set REG_Buffer, 29
 
 backup
 
 # alloc temp buffer
-  li r3, REQ_ALLSTAR_NUM_BUFSIZE    # this is larger, using this to alloc the buffer
+  li r3, GET_ADV_BUFSIZE    # this is larger, using this to alloc the buffer
   branchl r0, HSD_MemAlloc
   mr REG_Buffer, r3
 
-RequestOrder:
+RequestRNG:
 # cmd
-  li r3,CMD_REQ_ALLSTAR_RNG
-  stb r3,REQ_ALLSTAR_CMD_OFFSET(REG_Buffer)
-# difficulty
-  lbz r3, 0x0 (REG_MajorData)
-  rlwinm	r3, r3, 29, 27, 31
-  stb r3,REQ_ALLSTAR_RNG_NUM_OFFSET(REG_Buffer)
+  li r3,CMD_REQ_ADV_DATA
+  stb r3,REQ_ADV_CMD_OFFSET(REG_Buffer)
 # req
   mr r3,REG_Buffer
-  li  r4,REQ_ALLSTAR_NUM_BUFSIZE                #Length
+  li  r4,REQ_ADV_BUFSIZE                #Length
   li  r5,CONST_ExiWrite
   branchl r12,FN_EXITransferBuffer
 ReceiveOrder:
   mr r3,REG_Buffer
-  li  r4,GET_ALLSTAR_RNG_BUFSIZE                #Length
+  li  r4,GET_ADV_BUFSIZE                #Length
   li  r5,CONST_ExiRead
   branchl r12,FN_EXITransferBuffer
+  cmpwi r3, 1
+  beq RestoreRNG
+  cmpwi r3, -4                               # keep checking
+  ble ReceiveOrder
+  b Exit
 
+RestoreRNG:
 # update seed
   lwz	r3, -0x570C (r13)
-  lwz r0, GET_ALLSTAR_RNG_OFFSET (REG_Buffer)
+  lwz r0, GET_ADV_RNG_OFFSET (REG_Buffer)
   stw r0,0x0(r3)
 
+Exit:
 # free buffer
   mr r3, REG_Buffer
   branchl r0, 0x8037f1b0
 
-Exit:
   restore
-  lmw	r26, 0x0058 (sp)
+  addi	r3, r30, 0
