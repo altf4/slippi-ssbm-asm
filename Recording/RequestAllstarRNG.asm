ce0461be504563e970f0cf0c182cd5beb746b693
Justin S <Ownasaurus@gmail.com>
2022-09-05 21:12:00 -0400

    added luigi cyclone manip for stage 8

--- Recording/RequestAllstarRNG.asm
+++ Recording/RequestAllstarRNG.asm
@@ -14,6 +14,8 @@ backup
 # get index and store in r3
   lbz r3, 0x0 (REG_MajorData)
   rlwinm	r3, r3, 29, 27, 31
+# save a copy of the index in r5
+  addi r5, r3, 0
 
 # get the appropriate RNG seed into r0
   bl data          # branch to label
@@ -21,20 +23,29 @@ backup
   mulli r3,r3,0x4  # multiple index by 4, since each RNG seed is 4 bytes long
   lwzx r0,r3,r4    # access the r3'rd of the array
 
+# update luigi cyclone flag if necessary
+  cmpwi r5, 7 # index 7 is the stage we want to intercept
+  bne past_data # if it's not stage 7, move along
+  # update luigi cyclone flag
+  load r3, 0x80453080 # p1 data block
+  lwz r3, 0xB0(r3) # entity struct
+  lwz r3, 0x2C(r3) # character data
+  load r0, 0x00000000 # set the value to write
+  stw r0, 0x222C(r3) # save the value to the misc_flags offset
   b past_data           # don't execute the data as an instruction!
 
 # THANK YOU UnclePunch!
 data:
   blrl                                    # instantly return
-  .long 0xC88F4F11, 0xBB048EE8, 0x5939db62, 0x05c986e0, 0x5aa0be8c, 0x8b86cd40, 0xc8b6cbc9, 0x7584f37b, 0x9844996a, 0xc5d6059e, 0x9d077f22, 0xe6615017, 0x1084a48e
+  .long 0xC88F4F11, 0xB1C6920B, 0xBCFC16D1, 0x98E3A1B5, 0x7e225291, 0xe105b595, 0x74bd6922, 0xa6e1d15c, 0xfae86527, 0xc5d6059e, 0x9d077f22, 0xe6615017, 0x1084a48e
+  #        marth     bowser       kirby        yoshi       peach      gannon       doc          ness      y link       pichu       mario         ics      gnw
   .align 2                                # align so assembler doesnt complain
 
 past_data:
 # update seed
   lwz	r3, -0x570C (r13)
   stw r0,0x0(r3)
-
+  
 Exit:
   restore
   lmw	r26, 0x0058 (sp)
-  
