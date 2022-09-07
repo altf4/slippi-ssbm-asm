0d995c82c51ea594e2946ea223020f8aa5e16a0b
Justin S <Ownasaurus@gmail.com>
2022-09-06 23:40:53 -0400

    added more seeds, added UP's luigi fix

--- Recording/RequestAllstarRNG.asm
+++ Recording/RequestAllstarRNG.asm
@@ -24,20 +24,20 @@ backup
   lwzx r0,r3,r4    # access the r3'rd of the array
 
 # update luigi cyclone flag if necessary
-  cmpwi r5, 7 # index 7 is the stage we want to intercept
-  bne past_data # if it's not stage 7, move along
+  #cmpwi r5, 7 # index 7 is the stage we want to intercept
+  #bne past_data # if it's not stage 7, move along
   # update luigi cyclone flag
-  load r3, 0x80453080 # p1 data block
-  lwz r3, 0xB0(r3) # entity struct
-  lwz r3, 0x2C(r3) # character data
-  load r0, 0x00000000 # set the value to write
-  stw r0, 0x222C(r3) # save the value to the misc_flags offset
+  #load r3, 0x80453080 # p1 data block
+  #lwz r3, 0xB0(r3) # entity struct
+  #lwz r3, 0x2C(r3) # character data
+  #load r0, 0x00000000 # set the value to write
+  #stw r0, 0x222C(r3) # save the value to the misc_flags offset
   b past_data           # don't execute the data as an instruction!
 
 # THANK YOU UnclePunch!
 data:
   blrl                                    # instantly return
-  .long 0xC88F4F11, 0xB1C6920B, 0xBCFC16D1, 0x98E3A1B5, 0x7e225291, 0xe105b595, 0x74bd6922, 0xa6e1d15c, 0xfae86527, 0xc5d6059e, 0x9d077f22, 0xe6615017, 0x1084a48e
+  .long 0xC88F4F11, 0xB1C6920B, 0xBCFC16D1, 0x98E3A1B5, 0x7e225291, 0xe105b595, 0x74bd6922, 0xa6e1d15c, 0xfae86527, 0x98fc85e9, 0xa43b295d, 0xc5c0b749, 0x1084a48e
   #        marth     bowser       kirby        yoshi       peach      gannon       doc          ness      y link       pichu       mario         ics      gnw
   .align 2                                # align so assembler doesnt complain
 
